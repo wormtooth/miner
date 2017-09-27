721a249a505f4c455039f5e95a4f572fe8a6d7c1
wormtooth <thomasyrq@gmail.com>
2017-09-27 08:09:12 -0400

    add proxy and timeout support

--- miner.py
+++ miner.py
@@ -2,21 +2,28 @@ import requests
 import lxml.html as xhtml
 
 HEADERS = {
-    'User-Agent': "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0)",
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
+    'user-agent': "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0)",
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'connection': 'close'}
+TIMEOUT = 60
 
 
 class Miner(list):
+
     def __init__(self, url,
                  headers=HEADERS,
                  method='get',
                  payload={},
-                 cookies={}):
+                 cookies={},
+                 proxies={},
+                 timeout=TIMEOUT):
         self.url = url
         self.headers = headers
         self.method = method
         self.payload = payload
         self.cookies = cookies
+        self.proxies = proxies
+        self.timeout = timeout
         super(list, self).__init__()
 
         self.fetch()
@@ -34,11 +41,21 @@ class Miner(list):
             req = requests.get(url=self.url,
                                headers=self.headers,
                                cookies=self.cookies,
-                               params=self.payload)
+                               params=self.payload,
+                               proxies=self.proxies,
+                               timeout=self.timeout)
         else:
             req = requests.post(url=self.url,
                                 headers=self.headers,
                                 cookies=self.cookies,
-                                data=self.payload)
+                                data=self.payload,
+                                proxies=self.proxies,
+                                timeout=self.timeout)
         req.raise_for_status()
         self.build(req)
+
+
+class RawMiner(Miner):
+
+    def build(self, req):
+        self.parse(req)
